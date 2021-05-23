ae38d5f3b7c15a9f945a06dc6f099aa2e4d3dc80
Ilya <ilia.remezovsky@gmail.com>
2021-05-23 12:03:43 +0300

    Add info in text.json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        
-    }
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+  }
+}
+
+if (condition) {
+} else {
 }
